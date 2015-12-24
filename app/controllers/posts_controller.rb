5125bd4c85c570fa404a5561d79c73ca91430cef
LuzSb <nour.sboui1993@gmail.com>
2015-12-24 17:07:37 +0100

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,21 @@
+
 class PostsController < ApplicationController
-before_action :find_post, only: [:show, :edit, :update, :destroy]	
+before_action :find_post, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, except: [:index, :show]
 def index
 @posts = Post.all.order("created_at DESC")
 end
 def show
 end
 def new
-@post = current_user.posts.build
-end
+	@post = current_user.posts.build
+end	
 def create
 @post = current_user.posts.build(post_params)
 if @post.save
-redirect_to @post
+	redirect_to @post
 else
-    ender 'new'
+	render 'new'
 end
 end
 def edit
@@ -29,12 +31,14 @@ def destroy
 	@post.destroy
 	redirect_to root_path
 end
-	
-private
+private 
+
 def find_post
-	@post = Post.find(params[:id])
+		@post= Post.find(params[:id])
 end
+
 def post_params
 	params.require(:post).permit(:title, :content)
 end
 end
+
